392fea959487de2fc1bb4931d6f8c35abf27febf
Jan-Oliver Brandt <jan.oliver.brandt@me.com>
2022-04-07 14:57:41 +0200

    Re-Factored keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,59 +1,70 @@
-vim.cmd('let mapleader = " "')
-vim.cmd('map <space> <Nop>')
-vim.cmd('nmap <Leader>s /')
-vim.cmd('nnoremap <Leader>o o<Esc>')
-vim.cmd('nnoremap th :tabfirst<CR>')
-vim.cmd('nnoremap tk :tabnext<CR>')
-vim.cmd('nnoremap tj :tabprev<CR>')
-vim.cmd('nnoremap tl :tablast<CR>')
-vim.cmd('nnoremap tt :tabedit<Space>')
-vim.cmd('nnoremap tn :tabnew<CR>')
-vim.cmd('nnoremap td :tabclose<CR>')
-vim.cmd('nnoremap <leader>q :q<CR>')
-vim.cmd('nnoremap <leader><Esc> :q!<CR>')
-vim.cmd('nnoremap <leader>h :wincmd h<CR>')
-vim.cmd('nnoremap <leader>j :wincmd j<CR>')
-vim.cmd('nnoremap <leader>k :wincmd k<CR>')
-vim.cmd('nnoremap <leader>l :wincmd l<CR>')
-vim.cmd('nnoremap <silent> + :vertical resize +15<CR>')
-vim.cmd('nnoremap <silent> - :vertical resize -15<CR>')
-vim.cmd('nnoremap j jzz')
-vim.cmd('nnoremap k kzz')
-vim.cmd('nnoremap G Gzz')
-vim.cmd('nnoremap <S-j> <C-d>zz')
-vim.cmd('nnoremap <S-k> <C-u>zz')
-vim.cmd('nnoremap bn :bnext<CR>')
-vim.cmd('nnoremap bv :bprev<CR>')
-vim.cmd('nnoremap bd :bd<CR>')
-vim.cmd('noremap <esc> :noh <CR>')
-vim.cmd('nnoremap <leader>p :Gitsigns preview_hunk<CR>')
-vim.cmd('nmap <leader>gs :G<CR>')
-vim.cmd('nmap <leader>gc :G commit<CR>')
-vim.cmd('nmap <leader>gm :G merge ')
-vim.cmd('nmap <leader>go :G checkout ')
-vim.cmd('nmap <leader>gj :diffget //3<CR>')
-vim.cmd('nmap <leader>gf :diffget //2<CR>')
-vim.cmd('nmap <leader>gr wdv<CR><leader>k:q!<CR><leader>l')
-vim.cmd('nmap <leader>gl :G log<CR>')
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+
+map("n", "<Space>", "<Nop>", opt)
+map("n", "<Leader>o", "o<Esc>", opt)
+
+-- Search Mapping
+map("n", "<Leader>s", "/", opt)
+map("n", "<esc>", ":noh <CR>", opt)
+
+-- Tab Handling
+map("n", "th", ":tabfirst<CR>", opt)
+map("n", "tk", ":tabnext<CR>", opt)
+map("n", "tj", ":tabprev<CR>", opt)
+map("n", "tl", ":tablast<CR>", opt)
+map("n", "tt", ":tabedit<Space>", opt)
+map("n", "tn", ":tabnew<CR>", opt)
+map("n", "td", ":tabclose<CR>", opt)
+
+-- Buffer Handling
+map("n", "<leader>q", ":q<CR>", opt)
+map("n", "<leader><Esc>", ":q!<CR>", opt)
+map("n", "bn", ":bnext<CR>", opt)
+map("n", "bv", ":bprev<CR>", opt)
+map("n", "bd", ":bd<CR>", opt)
+
+-- Window Management
+map("n", "<leader>h", ":wincmd h<CR>", opt)
+map("n", "<leader>j", ":wincmd j<CR>", opt)
+map("n", "<leader>k", ":wincmd k<CR>", opt)
+map("n", "<leader>l", ":wincmd l<CR>", opt)
+map("n", "<silent>", "+ :vertical resize +15<CR>", opt)
+map("n", "<silent>", "- :vertical resize -15<CR>", opt)
+
+-- Navigation
+map("n", "j", "jzz", opt)
+map("n", "k", "kzz", opt)
+map("n", "G", "Gzz", opt)
+map("n", "<S-j>", "<C-d>zz", opt)
+map("n", "<S-k>", "<C-u>zz", opt)
 
 
--- vim.cmd('IndentBlanklineDisable')
-vim.cmd('nnoremap <C-i> :IndentBlanklineToggle<CR>')
-vim.cmd('nnoremap <leader>b :NvimTreeToggle<CR>')
-vim.cmd('nnoremap <leader>o :Telescope projects<CR>')
-vim.cmd('nnoremap <leader>ff <cmd>Telescope find_files<cr>')
-vim.cmd('nnoremap <leader>fg <cmd>Telescope live_grep<cr>')
-vim.cmd('nnoremap <leader>fb <cmd>Telescope buffers<cr>')
-vim.cmd('nnoremap <leader>fh <cmd>Telescope help_tags<cr>')
+-- Git Handling
+map("n", "<leader>p", ":Gitsigns preview_hunk<CR>", opt)
+map("n", "<leader>gs", ":G<CR>", opt)
+map("n", "<leader>gc", ":G commit<CR>", opt)
+map("n", "<leader>gm", ":G merge ", opt)
+map("n", "<leader>go", ":G checkout ", opt)
+map("n", "<leader>gj", ":diffget //3<CR>", opt)
+map("n", "<leader>gf", ":diffget //2<CR>", opt)
+map("n", "<leader>gr", "wdv<CR><leader>k:q!<CR><leader>l", opt)
+map("n", "<leader>gl", ":G log<CR>", opt)
 
-vim.cmd('highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080')
-vim.cmd('highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6')
-vim.cmd('highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6')
-vim.cmd('highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE')
-vim.cmd('highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE')
-vim.cmd('highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE')
-vim.cmd('highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0')
-vim.cmd('highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0')
-vim.cmd('highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4')
-vim.cmd('highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4')
-vim.cmd('highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4')
+-- Fuzzy Finding
+map("n", "<leader>ff", ":Telescope find_files<cr>", opt)
+map("n", "<leader>fg", ":Telescope live_grep<cr>", opt)
+map("n", "<leader>fb", ":Telescope buffers<cr>", opt)
+map("n", "<leader>fh", ":Telescope help_tags<cr>", opt)
+map("n", "<leader>o",  ":Telescope projects<CR>", opt)
+
+
+-- File Explorer
+map("n", "<leader>b", ":NvimTreeToggle<CR>", opt)
+
+-- Appereance
+map("n", "<C-i>", ":IndentBlanklineToggle<CR>", opt)
+
